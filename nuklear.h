df0e16750aba4a4c1c3aae3f8fca71438ab792ec
Alexander W. Schultz <alexschultz002@gmail.com>
2024-11-14 08:07:56 -0500

    am dumb. forgot paq. should probably make it a git hook

--- nuklear.h
+++ nuklear.h
@@ -22150,6 +22150,7 @@ nk_style_pop_##type(struct nk_context *ctx)\
     element = &type_stack->elements[--type_stack->head];\
     *element->address = element->old_value;\
     return 1;\
+}
 /**
  * \brief <fill in>
  *
@@ -22162,7 +22163,6 @@ nk_style_pop_##type(struct nk_context *ctx)\
  *
  * \returns <fill in>
  */
-}
 NK_API nk_bool NK_STYLE_PUSH_IMPLEMENATION(struct nk, style_item, style_items)
 NK_API nk_bool NK_STYLE_PUSH_IMPLEMENATION(nk,float, floats)
 NK_API nk_bool NK_STYLE_PUSH_IMPLEMENATION(struct nk, vec2, vectors)
