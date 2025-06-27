09a183b1e80c249aaa5b6732447e2936319f825a
Alexander W. Schultz <alexschultz002@gmail.com>
2025-06-27 09:43:41 -0400

    ncurses is working

---
+++ demo/ncurses/main.c
@@ -0,0 +1,210 @@
+/* nuklear - v1.32.0 - public domain */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <time.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_IMPLEMENTATION
+#define NK_XLIB_IMPLEMENTATION
+#include "../../nuklear.h"
+/*#include "nuklear_xlib.h"*/
+#include <ncurses.h>
+
+#define DTIME           20
+#define WINDOW_WIDTH    800
+#define WINDOW_HEIGHT   600
+
+
+static void
+die(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    fputs("\n", stderr);
+    exit(EXIT_FAILURE);
+}
+
+static long
+timestamp(void)
+{
+    struct timeval tv;
+    if (gettimeofday(&tv, NULL) < 0) return 0;
+    return (long)((long)tv.tv_sec * 1000 + (long)tv.tv_usec/1000);
+}
+
+static void
+sleep_for(long t)
+{
+    struct timespec req;
+    const time_t sec = (int)(t/1000);
+    const long ms = t - (sec * 1000);
+    req.tv_sec = sec;
+    req.tv_nsec = ms * 1000000L;
+    while(-1 == nanosleep(&req, &req));
+}
+
+/* ===============================================================
+ *
+ *                          EXAMPLE
+ *
+ * ===============================================================*/
+/* This are some code examples to provide a small overview of what can be
+ * done with this library. To try out an example uncomment the defines */
+/*#define INCLUDE_ALL */
+/*#define INCLUDE_STYLE */
+/*#define INCLUDE_CALCULATOR */
+/*#define INCLUDE_CANVAS */
+#define INCLUDE_OVERVIEW
+/*#define INCLUDE_CONFIGURATOR */
+/*#define INCLUDE_NODE_EDITOR */
+
+#ifdef INCLUDE_ALL
+  #define INCLUDE_STYLE
+  #define INCLUDE_CALCULATOR
+  #define INCLUDE_CANVAS
+  #define INCLUDE_OVERVIEW
+  #define INCLUDE_CONFIGURATOR
+  #define INCLUDE_NODE_EDITOR
+#endif
+
+#ifdef INCLUDE_STYLE
+  #include "../../demo/common/style.c"
+#endif
+#ifdef INCLUDE_CALCULATOR
+  #include "../../demo/common/calculator.c"
+#endif
+#ifdef INCLUDE_CANVAS
+  #include "../../demo/common/canvas.c"
+#endif
+#ifdef INCLUDE_OVERVIEW
+  #include "../../demo/common/overview.c"
+#endif
+#ifdef INCLUDE_CONFIGURATOR
+  #include "../../demo/common/style_configurator.c"
+#endif
+#ifdef INCLUDE_NODE_EDITOR
+  #include "../../demo/common/node_editor.c"
+#endif
+
+/* ===============================================================
+ *
+ *                          DEMO
+ *
+ * ===============================================================*/
+int main(void)
+{
+    long dt;
+    long started;
+    int running = 1;
+    struct nk_context ctx;
+
+    #ifdef INCLUDE_CONFIGURATOR
+    static struct nk_color color_table[NK_COLOR_COUNT];
+    memcpy(color_table, nk_default_color_style, sizeof(color_table));
+    #endif
+
+    /* since tui is character sized pixels, we just return the length of the
+     text as if they are pixels.*/
+      float your_text_width_calculation(nk_handle handle, float height, const char *text, int len)
+      {
+          return len;
+      }
+ 
+      struct nk_user_font font;
+      font.userdata.ptr = NULL;
+      font.height = 1;
+      font.width = your_text_width_calculation;
+ 
+    nk_init_default(&ctx, 0 );
+
+    while (running)
+    {
+        initscr();
+        printw("help me");
+        refresh();
+        getch();
+        running = 0;
+
+        /* Input */
+
+#if 0
+        XEvent evt;
+        started = timestamp();
+        nk_input_begin(ctx);
+        while (XPending(xw.dpy)) {
+            XNextEvent(xw.dpy, &evt);
+            if (evt.type == ClientMessage) goto cleanup;
+            if (XFilterEvent(&evt, xw.win)) continue;
+            nk_xlib_handle_event(xw.dpy, xw.screen, xw.win, &evt);
+        }
+        nk_input_end(ctx);
+
+        /* GUI */
+        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 200, 200),
+            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+            NK_WINDOW_CLOSABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        {
+            enum {EASY, HARD};
+            static int op = EASY;
+            static int property = 20;
+
+            nk_layout_row_static(ctx, 30, 80, 1);
+            if (nk_button_label(ctx, "button"))
+                fprintf(stdout, "button pressed\n");
+            nk_layout_row_dynamic(ctx, 30, 2);
+            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+            nk_layout_row_dynamic(ctx, 25, 1);
+            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+        }
+        nk_end(ctx);
+        if (nk_window_is_hidden(ctx, "Demo")) break;
+
+        /* -------------- EXAMPLES ---------------- */
+        #ifdef INCLUDE_CALCULATOR
+          calculator(ctx);
+        #endif
+        #ifdef INCLUDE_CANVAS
+          canvas(ctx);
+        #endif
+        #ifdef INCLUDE_OVERVIEW
+          overview(ctx);
+        #endif
+        #ifdef INCLUDE_CONFIGURATOR
+          style_configurator(ctx, color_table);
+        #endif
+        #ifdef INCLUDE_NODE_EDITOR
+          node_editor(ctx);
+        #endif
+        /* ----------------------------------------- */
+
+        /* Draw */
+        XClearWindow(xw.dpy, xw.win);
+        nk_xlib_render(xw.win, nk_rgb(30,30,30));
+        XFlush(xw.dpy);
+
+        /* Timing */
+        dt = timestamp() - started;
+        if (dt < DTIME)
+            sleep_for(DTIME - dt);
+#endif
+    }
+
+
+cleanup:
+    endwin();
+    return 0;
+}
+
